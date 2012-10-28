4dfbd856fa10c0d01ab7cefd07dbd328cd5fb7e8
Lixiang Jiang <lixiang.jiang@gmail.com>
2012-10-27 21:34:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CLN::Application.config.secret_token = '265457718984e31c2f9f5f48ed61820aad67fa72bd2d99667aad7a05b626acb4baa7a14cf84ed4fef5eb68e9ff26ebc12fdca9113312d2cd5f776a8e9253573c'
